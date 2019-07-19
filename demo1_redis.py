c3a8c84b721440a837e1ca8298cb96619f873d2a
libo <15102978312@163.com>
2019-07-19 15:30:25 +0800

    add:redis字符串查询和删除方法

--- demo1_redis.py
+++ demo1_redis.py
@@ -9,15 +9,27 @@ class RedisTest(object):
     def __init__(self):
         self.sr = StrictRedis(host="10.61.121.80", port=6379)
 
-    def add_data(self):
-        key = "name"
-        value = "li"
-        self.sr.set(key, value)
+    def add_data(self, key, value):
+        """
+        redis字符串类型添加方法
+        """
+        return self.sr.set(key, value)
+
+    def get_data(self, key):
+        return self.sr.keys(key)
+
+    def delete_data(self, key):
+        return self.sr.delete(key)
 
 
 if __name__ == '__main__':
     try:
         rt = RedisTest()
-        print(rt.add_data())
+        a = 'name'
+        b = 'Jack'
+        rt.add_data(a, b)
+        name = 'name'
+        print(rt.get_data(name))
+        print(rt.delete_data(name))
     except Exception as e:
         print(e)
